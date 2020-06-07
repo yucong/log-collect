d3066c83bc2dfa9de53a84e0ad81d8fd9fada59b
= <yucong@laibai.net>
2020-06-07 21:11:14 +0800

    代码提交

---
+++ log-manager/src/main/java/com/yucong/log/LogManagerApplication.java
@@ -0,0 +1,13 @@
+package com.yucong.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LogManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogManagerApplication.class, args);
+	}
+}
--- log-manager/src/test/java/com/yucong/log/ApplicationTests.java
+++ log-manager/src/test/java/com/yucong/log/ApplicationTests.java
@@ -1,14 +1,14 @@
-package com.website;
+package com.yucong.log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.yucong.log.Application;
+import com.yucong.log.LogManagerApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = LogManagerApplication.class)
 public class ApplicationTests {
 
 	@Test
